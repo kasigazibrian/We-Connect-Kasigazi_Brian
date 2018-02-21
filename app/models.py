1c97746fcf5742bb097d9f198e16e7a4dd60256d
KASIGAZI BRIAN <kasigazibrian>
2018-02-21 15:32:54 +0300

    modifications to the data structure

--- app/models.py
+++ app/models.py
@@ -7,15 +7,12 @@ class User(object):
         self.username = username
         self.password = password
 
-    def __str__(self):
-        return "User(id='%s')" % self.id
+    # def __str__(self):
+    #     return "User(id='%s')" % self.id
+#users=[User(1,'brian','abcd'),User(2,'Dan','abcdef')]
 
-users = [
-    User(1, 'brian', 'abcd'),
-    User(2, 'gita', 'abcdef'),
-]
 
-class business(object):
+class Business(object):
     def __init__(self, id, businessname, businesslocation, businesscategory, contactnumber, email):
         self.id= id
         self.businessname= businessname
@@ -25,4 +22,23 @@ class business(object):
         self.email = email
 
     def __str__(self):
-        return "User(id='%s')" % self.id
+        return "Business(id='%s')" % self.id
+
+class Signup(object):
+    def __init__(self,id,firstname, lastname, username, password,email, gender):
+        self.id = id
+        self.firstname = firstname
+        self.lastname=lastname
+        self.username = username
+        self.password = password
+        self.email = email
+        self.gender = gender
+    def __str__(self):
+        return "Signup(id='%s')" % self.id
+class Reviews(object):
+    def __init__(self, id, review):
+        self.id = id
+        self.review = review
+
+    def __str__(self):
+        return "Reviews(id='%s')" % self.id
