5f9fd61be97ab0924fdf6ed40559c2e8a8856b7c
KASIGAZI BRIAN <kasigazibrian>
2018-02-22 02:23:07 +0300

    adds modifications to models.py to create relationships between businesses and reviews, businesses and the business owners

--- app/models.py
+++ app/models.py
@@ -1,44 +1,60 @@
-from flask import jsonify
-from flask_jwt import JWT, jwt_required, current_identity
+"""Models.py"""
+
 
 class User(object):
-    def __init__(self, id, username, password):
-        self.id = id
+    """User class for logging in a new user"""
+    def __init__(self, user_id, username, password, message, login_status):
+        self.user_id = user_id
         self.username = username
         self.password = password
+        self.message = message
+        self.login_status = login_status
 
-    # def __str__(self):
-    #     return "User(id='%s')" % self.id
-#users=[User(1,'brian','abcd'),User(2,'Dan','abcdef')]
+    def __str__(self):
+        return "Business(user_id='%s')" % self.user_id
 
 
 class Business(object):
-    def __init__(self, id, businessname, businesslocation, businesscategory, contactnumber, email):
-        self.id= id
-        self.businessname= businessname
-        self.businesslocation = businesslocation
-        self.businesscategory = businesscategory
-        self.contactnumber = contactnumber
+    """Business class for registering a new business"""
+    def __init__(self, business_id, business_owner_id, business_name, business_location,
+                 business_category, contact_number, email, message):
+        self.business_id = business_id
+        self.business_owner_id = business_owner_id
+        self.business_name = business_name
+        self.business_location = business_location
+        self.business_category = business_category
+        self.contact_number = contact_number
         self.email = email
+        self.message = message
 
     def __str__(self):
-        return "Business(id='%s')" % self.id
+        return "Business(business_id='%s')" % self.business_id
+
 
 class Signup(object):
-    def __init__(self,id,firstname, lastname, username, password,email, gender):
-        self.id = id
-        self.firstname = firstname
-        self.lastname=lastname
+    """Signup class for registering a new user"""
+    def __init__(self, new_user_id, first_name, last_name, username, password,
+                 email, gender, message):
+        self.new_user_id = new_user_id
+        self.first_name = first_name
+        self.last_name = last_name
         self.username = username
         self.password = password
         self.email = email
         self.gender = gender
-    def __str__(self):
-        return "Signup(id='%s')" % self.id
-class Reviews(object):
-    def __init__(self, id, review):
-        self.id = id
-        self.review = review
+        self.message = message
 
     def __str__(self):
-        return "Reviews(id='%s')" % self.id
+        return "Signup(id='%s')" % self.new_user_id
+
+
+class Reviews(object):
+    """Reviews class for creating a review"""
+    def __init__(self, review_id, business_id, review, message):
+        self.review_id = review_id
+        self.review = review
+        self.business_id = business_id
+        self.message = message
+
+    def __str__(self):
+        return "Reviews(id='%s')" % self.review_id
