1b3c2d194134ef783c828a26ea84eae0f20978d9
KASIGAZI BRIAN <kasigazibrian>
2018-02-22 02:27:37 +0300

    adds modifications to tests.py to enable testing API end points

--- tests.py
+++ tests.py
@@ -1,88 +1,147 @@
 """tests.py"""
 from app import app
-from flask import jsonify
-import os
-from app.models import User
 import unittest
 import json
 
 
-
-class UserTestCase(unittest.TestCase):
-    """This class represents the """
+class FlaskTestCase(unittest.TestCase):
+    """This class represents the flask test cases"""
 
     def create_app(self):
+        """Initialize the app"""
         app.config['TESTING'] = True
         return app
 
     def setUp(self):
         """Define test variables and initialize app."""
         self.client = app.test_client(self)
-        self.user = {
-            'id': '1',
-            'firstname': 'Moses',
-            'lastname': 'Magezi',
+        self.users = [{
+            'new_user_id': '1',
+            'first_name': 'Moses',
+            'last_name': 'Mason',
             'email': 'moses@gmail.com',
             'username': 'moses',
             'password': 'banana',
-            'gender': 'male'
-        }
+            'gender': 'male',
+            'message': ''
+        },
+            {
+                'new_user_id': '2',
+                'first_name': 'Mable',
+                'last_name': 'Sarah',
+                'email': 'mabel@gmail.com',
+                'username': 'Mable',
+                'password': 'pineapple',
+                'gender': 'female',
+                'message': ''
+         }
+        ]
         self.login ={
-            'id':'1',
-            'username':'Mary',
-            'password': '12345'
+            'user_id': '1',
+            'username': 'Mary',
+            'password': '12345',
+            'message': '',
+            'login_status': ''
         }
-        self.business = {
-            'id': '1',
-            'businessname': 'supercom',
-            'businesslocategory': 'entertainment',
-            'businesslocation': 'kampala',
+        self.businesses = [{
+            'business_id': '1',
+            'business_owner_id': '1',
+            'business_name': 'media studios',
+            'business_category': 'entertainment',
+            'business_location': 'kampala',
             'email': 'supercom@gmail.com',
-            'contactnumber': '256781712929'
-        }
+            'contact_number': '256781712929'
+        },
+            {
+                'business_id': '2',
+                'business_owner_id': '2',
+                'business_name': 'real houses',
+                'business_category': 'real estate',
+                'business_location': 'kabale',
+                'email': 'real@gmail.com',
+                'contact_number': '256781712928'
+            },
+        ]
+        self.reviews =[{
+            'review_id':'1',
+            'business_id':'1',
+            'review':'This business is awesome',
+            'message': '',
+        },
+            {
+                'review_id': '2',
+                'business_id': '2',
+                'review': 'This business rocks',
+                'message': '',
+        },
+            {
+                'review_id': '3',
+                'business_id': '2',
+                'review': 'They have poor customer care',
+                'message': '',
 
-
+            }]
 
 
     def test_API_can_create_a_user_account(self):
         """Tests if a user account is created"""
         tester = app.test_client(self)
-        response = tester.post("/api/auth/register", data=json.dumps(self.user))
-        self.assertEqual(response.status_code, 200)
-        self.assertIn('Moses', str(response.data))
+
+        response = tester.post("/api/auth/register", data=json.dumps(self.users))
+        with response as res:
+            self.assertEqual(res.status_code, 201)
+            self.assertIn('user created successfully', str(res.data))
 
     def test_log_in_post_request(self):
-        """tests if a user is logged in(to be modified)"""
+        """Tests if a user is logged in"""
         tester = app.test_client(self)
-        response = tester.post("/api/auth/login")
-        self.assertEqual(response.status_code, 200)
-        self.assertIn('Mary', str(response.data))
+        response = tester.post("/api/auth/login", data=json.dumps(self.login))
+        self.assertEqual(response.status_code, 201)
+        self.assertIn('true', str(response.data))
 
     def test_logout_post_request(self):
+        """Tests if a user logs out"""
         tester = app.test_client(self)
-        response = tester.post("/api/auth/logout", data=self.login)
+        response = tester.post("/api/auth/logout", data=json.dumps(self.login))
         self.assertEqual(response.status_code, 200)
-        self.assertIn('Mary', str(response.data))
+        self.assertIn('false', str(response.data))
 
     def test_password_reset_post_request(self):
+        """Tests if the new password for a user is obtained"""
         tester = app.test_client(self)
-        response = tester.post("/api/auth/reset-password", data=self.login)
+        # Test using user Moses and change his password from banana to oranges
+        response = tester.post("/api/auth/reset-password", data=json.dumps(self.users))
         self.assertEqual(response.status_code, 200)
-        self.assertIn('Mary', str(response.data))
+        self.assertIn('oranges', str(response.data))
 
     def test_API_can_create_a_business(self):
-        """Tests if a user account is created"""
+        """Tests if a business is created"""
         tester = app.test_client(self)
-        response = tester.post("/api/auth/register", data=json.dumps(self.user))
+        response = tester.post("/api/businesses", data=json.dumps(self.businesses))
+        self.assertEqual(response.status_code, 201)
+        self.assertIn('business created successfully', str(response.data))
+
+    def test_API_can_update_a_business(self):
+        """Tests if a business is updated(to be modified)"""
+        tester = app.test_client(self)
+        response = tester.put("/api/businesses/<business_id>", data=json.dumps(self.businesses))
         self.assertEqual(response.status_code, 200)
         self.assertIn('Moses', str(response.data))
 
-    def test_API_can_update_a_business(self):
-        """Tests if a user account is created"""
+    def test_API_can_post_a_review(self):
+        """Tests if a business is deleted(to be modified)"""
         tester = app.test_client(self)
-        response = tester.put("/api/businesses/<business_id>", data=json.dumps(self.user))
+        response = tester.post("/api/businesses/<business_id>/reviews", data=json.dumps(self.businesses))
         self.assertEqual(response.status_code, 200)
-        self.assertIn('Moses', str(response.data))
+        self.assertIn('', str(response.data))
+
+    def test_API_can_post_reviews(self):
+        """Tests if a business review is added"""
+        tester = app.test_client(self)
+        response = tester.post("/api/businesses/<business_id>/reviews", data=json.dumps(self.reviews))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Review added successfully', str(response.data))
+
 
 if __name__=='__main__':
     unittest.main()
