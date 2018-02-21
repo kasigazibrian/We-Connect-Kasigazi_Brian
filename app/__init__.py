ae29ee7d98511a590b09bb350a86dc60c244dd39
KASIGAZI BRIAN <kasigazibrian>
2018-02-21 15:35:20 +0300

    Adds linting and pep8 standard

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,9 @@
-from flask import Flask, jsonify, request
+"""__init__.py"""
+from flask import Flask
 
-import os
+
 app = Flask(__name__)
 
+
 app.config.from_pyfile('../config.py')
-
-
 from app import views
-
