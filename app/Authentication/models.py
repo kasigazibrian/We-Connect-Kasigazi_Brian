f9da6e4360208b02077d10b1187e6ab170c6a443
KASIGAZI BRIAN <kasigazibrian>
2018-02-28 23:26:56 +0300

    adds the Authentication module

---
+++ app/Authentication/models.py
@@ -0,0 +1,73 @@
+from flask import jsonify, request, make_response
+from app import app
+import jwt, datetime
+
+class User(object):
+    """User class"""
+    def __init__(self, user_id, username, password, first_name, last_name, email, gender):
+        self.user_id = user_id
+        self.username = username
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.gender = gender
+
+    def __str__(self):
+        return "User(user_id='%s')" % self.user_id
+
+    @staticmethod
+    def register(new_user):
+        """Register a user"""
+        message = 'User registered successfully'
+        response = jsonify({
+            'user_id': new_user.user_id,
+            'username': new_user.username,
+            'password': new_user.password,
+            'first_name': new_user.first_name,
+            'last_name':new_user.last_name,
+            'email': new_user.email,
+            'gender': new_user.gender,
+            'message': 'User has been registered successfully'
+        })
+        response.status_code = 201
+        return response
+
+    @staticmethod
+    def logout(user_id):
+        """Logs out a user"""
+        my_user = User(user_id='1', username='moses', password='banana', first_name='Moses', last_name='Baguma',
+                       email='baguma@gmail.com', gender='male')
+        if user_id == my_user.user_id:
+            message = 'You have successfully logged out'
+            response = jsonify({'message': message})
+            response.status_code = 200
+            return response
+
+    @staticmethod
+    def login(username, password):
+        """Logs in a user"""
+        # create mock-up data to authenticate the user
+        my_user = User(user_id='1', username='moses',password='banana', first_name='Moses', last_name ='Baguma',
+                       email='baguma@gmail.com', gender='male')
+        if username == my_user.username:
+            if my_user.password == password:
+                token = jwt.encode({'username': username,
+                                    'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=40)},
+                                    app.config['SECRET_KEY'])
+                return jsonify({'token': token.decode('utf-8'),'message':'Token created successfully'})
+            else:
+                return make_response("could not verify", 401,
+                                             {'WWW-Authenticate': 'Basic realm="Login required"'})
+        else:
+            return make_response("could not verify", 401, {'WWW-Authenticate': 'Basic realm="Login required"'})
+
+
+    @staticmethod
+    def password_reset(new_password):
+        """reset user password"""
+        my_user = User(user_id=1, username='moses', password='banana', first_name='Moses', last_name='Baguma',
+                       email='baguma@gmail.com', gender='male')
+        my_user.password = new_password
+        message = "password has been reset"
+        return jsonify({'new_password': new_password, 'message': message})
---
+++ app/Authentication/views.py
@@ -0,0 +1,101 @@
+"""views.py"""
+from flask import request, jsonify, make_response
+from app import app, api
+from flask_restful import Resource
+from app.Authentication.models import User
+from functools import wraps
+import jwt
+
+
+def jwt_required(f):
+    @wraps(f)
+    def decorated(*args,**kwargs):
+        token = None
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+            if token:
+                try:
+                    data = jwt.decode(token, app.config['SECRET_KEY'])
+                    current_user = data['username']
+                except:
+                    return jsonify({'message': 'Token is missing'})
+            else:
+                return jsonify({'message': 'Token is missing'})
+        else:
+            return jsonify({'message': 'Token is missing'})
+        return f(current_user, *args, **kwargs)
+    return decorated
+
+
+class Home(Resource):
+    """Class for the home page"""
+    def get(self):
+        return {'message': 'Success'}
+
+
+class Register(Resource):
+    """Class for registering a user """
+    def post(self):
+        test_user = request.get_json(force=True)
+        user_id = test_user.get('user_id')
+        username = test_user.get('username')
+        password = test_user.get('password')
+        first_name = test_user.get('first_name')
+        last_name = test_user.get('last_name')
+        email = test_user.get('email')
+        gender = test_user.get('gender')
+        new_user = User(user_id=user_id, username=username, password=password, first_name=first_name,
+                        last_name=last_name,
+                        email=email, gender=gender)
+        response = User.register(new_user)
+        return response
+
+
+class Login(Resource):
+    """Class for logging in a user"""
+    def post(self):
+        """obtain user data sent in a post request"""
+        test_user = request.get_json(force=True)
+        username = test_user.get('username')
+        password = test_user.get('password')
+        res = User.login(username=username, password=password)
+        res.status_code = 201
+        return res
+
+
+class Logout(Resource):
+    """class for logging out a user"""
+    @jwt_required
+    def post(self, current_user):
+        # obtain user data sent in a post request
+        if current_user:
+            user_data = request.get_json(force=True)
+            user_id = user_data.get('user_id')
+            logout_res = User.logout(user_id=user_id)
+            return logout_res
+
+
+class ResetPassword(Resource):
+    """class for resetting user password"""
+    @jwt_required
+    def post(self, current_user):
+        if current_user:
+            new_password = request.get_json(force=True)
+            my_new_password = new_password.get('password')
+            if my_new_password:
+                response = User.password_reset(new_password=new_password)
+                # Call on the password_reset method
+                response.status_code = 200
+                return response
+            else:
+                return jsonify({'message': 'Failed to update password'})
+        else:
+            # return jsonify({'message': 'Please log in to access this page'})
+            return str(current_user.moses)
+
+
+api.add_resource(Home, '/')
+api.add_resource(Register,'/api/v1/auth/register')
+api.add_resource(Login,'/api/v1/login')
+api.add_resource(Logout,'/api/v1/auth/logout')
+api.add_resource(ResetPassword,'/api/v1/auth/reset-password')
