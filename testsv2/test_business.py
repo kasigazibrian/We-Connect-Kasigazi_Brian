70a1cc35f90bbd6907dd6c3ec30d1dca21d08db9
kasigazibrian <kasigazibrian@gmail.com>
2018-03-02 20:48:07 +0300

    Update test_business.py

--- testsv2/test_business.py
+++ testsv2/test_business.py
@@ -54,23 +54,23 @@ class FlaskTestCase(BaseTestCase):
     """Test class for business"""
     def test_API_can_create_a_business(self):
         """Tests if a business is created"""
-        response = self.client.post("/api/v1/businesses", data=json.dumps(self.businesses[0]))
+        response = self.client.post("/api/v2/businesses", data=json.dumps(self.businesses[0]))
         self.assertIn('business created successfully', str(response.data))
 
     def test_API_can_obtain_a_specific_business(self):
         """Tests if one can retrieve a specific business"""
         tester = app.test_client(self)
-        res = tester.post('/api/v1/businesses', data=json.dumps(self.businesses[0]))
+        res = tester.post('/api/v2/businesses', data=json.dumps(self.businesses[0]))
         self.assertIn('business created successfully', str(res.data))
-        res = tester.get('/api/v1/businesses/3')
+        res = tester.get('/api/v2/businesses/3')
         self.assertEqual(res.status_code, 200)
         self.assertIn('MyFi', str(res.data))
 
     def test_API_can_get_all_businesses(self):
         tester = app.test_client(self)
-        res = tester.post('/api/v1/businesses', data=json.dumps(self.businesses[0]))
+        res = tester.post('/api/v2/businesses', data=json.dumps(self.businesses[0]))
         self.assertIn('business created successfully', str(res.data))
-        response = tester.get("/api/v1/businesses", data=json.dumps(self.businesses[0]))
+        response = tester.get("/api/v2/businesses", data=json.dumps(self.businesses[0]))
         self.assertIn('media studios', str(response.data))
         self.assertIn('real houses', str(response.data))
         self.assertIn('MyFi', str(response.data))
@@ -78,17 +78,17 @@ class FlaskTestCase(BaseTestCase):
     def test_API_will_fail_to_delete_a_business_without_token(self):
         """Tests if a business is deleted(to be modified)"""
         tester = app.test_client(self)
-        response = tester.delete("/api/v1/businesses/1")
+        response = tester.delete("/api/v2/businesses/1")
         self.assertEqual(response.status_code, 200)
         self.assertIn('Token is missing', str(response.data))
 
     def test_API_will_not_update_business_without_token(self):
         """Tests if a business is updated(to be modified)"""
         tester = app.test_client(self)
-        response = tester.post("/api/v1/businesses", data=json.dumps(self.businesses[0]))
+        response = tester.post("/api/v2/businesses", data=json.dumps(self.businesses[0]))
         self.assertIn('business created successfully', str(response.data))
         result_in_json = json.loads(response.data.decode('utf-8').replace("'", "\""))
-        res = tester.put('/api/v1/businesses/{}'.format(result_in_json['business_id']), data=json.dumps(self.businesses[0]))
+        res = tester.put('/api/v2/businesses/{}'.format(result_in_json['business_id']), data=json.dumps(self.businesses[0]))
         self.assertEqual(res.status_code, 200)
         self.assertIn('Token is missing', str(res.data))
 
