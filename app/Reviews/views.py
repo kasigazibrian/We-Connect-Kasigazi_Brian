9e32976d3ac59dd7e2d2c93e8278ce2bc789117e
KASIGAZI BRIAN <kasigazibrian>
2018-03-23 03:12:19 +0300

    Modifies the import statement to import app from the file app created

--- app/Reviews/views.py
+++ app/Reviews/views.py
@@ -1,7 +1,7 @@
-from flask import request
+from flask import request, jsonify
 from flask_restful import Resource
-from app import  api
-from app.models import BusinessReviews
+from app.app import  api
+from app.models import BusinessReviews, Business
 
 
 class GetAllBusinessReviews(Resource):
@@ -15,9 +15,14 @@ class AddBusinessReview(Resource):
     def post(self, business_id):
         new_review_data = request.get_json(force=True)
         review = new_review_data.get('review')
-        new_business_review = BusinessReviews(review=review, business_id=business_id)
-        add_business_review = BusinessReviews.add_review(new_business_review)
-        return add_business_review
+        mybusiness = Business.query.filter_by(business_id=business_id).first()
+        if mybusiness:
+            new_business_review = BusinessReviews(review=review, business_id=business_id)
+            add_business_review = BusinessReviews.add_review(new_business_review)
+            return add_business_review
+        else:
+            return jsonify({"message":"Business to add a review to does not exist. Please ensure"
+                                      " that you have indicated the correct business id"})
 
 
 api.add_resource(GetAllBusinessReviews,'/api/v2/businesses/<business_id>/reviews')
