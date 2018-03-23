50ba3117ac69923ac5b7ab59385ae0b37de15e28
KASIGAZI BRIAN <kasigazibrian>
2018-03-23 03:02:36 +0300

    Adds modifications to manage.py to cater for the created file app.py

--- manage.py
+++ manage.py
@@ -1,14 +1,13 @@
-"""manage.py"""
-from runserver import app
+"""manage.py to handle falsk migrations"""
+from app.app import app
 from flask_script import Manager
-from app.models import db
 from flask_migrate import Migrate, MigrateCommand
+from app.models import db
 
 migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
-
 
 if __name__ == '__main__':
     manager.run()
