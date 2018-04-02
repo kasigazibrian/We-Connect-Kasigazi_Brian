e0be67e1cef1b16cdadb74c116b6e735f39c162f
KASIGAZI BRIAN <kasigazibrian>
2018-04-02 12:02:12 +0300

    Adds API description to swagger documentation

--- app/v1/__init__.py
+++ app/v1/__init__.py
@@ -6,7 +6,10 @@ app = Flask(__name__)
 
 app.config.from_object('config.DevelopmentConfig')
 
-api = Api(app)
+api = Api(app, description="WeConnect provides a platform that brings businesses and individuals together."
+                           "This platform creates awareness for businesses and gives the users the ability "
+                           "to write reviews about the businesses they have interacted with."
+                           " Below is the documentation for the WeConnect RESTFUL API built using flask-restplus")
 
 from app.v1.Authentication import views
 from app.v1.Business import views
