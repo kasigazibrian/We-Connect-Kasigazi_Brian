fea86c387e22740a8c2ff4dc4a9d92565cb7297d
KASIGAZI BRIAN <kasigazibrian>
2018-04-02 12:06:47 +0300

    Adds the Api documentation for swagger documentaion

--- app/app.py
+++ app/app.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_restful import Api
-
+from flask_restplus import Api
 
 app = Flask(__name__)
 
 app.config.from_object('config.DevelopmentConfig')
 
 db = SQLAlchemy(app)
-api = Api(app)
+
+api = Api(app, description="WeConnect provides a platform that brings businesses and individuals together."
+                           "This platform creates awareness for businesses and gives the users the ability "
+                           "to write reviews about the businesses they have interacted with."
+                           " Below is the documentation for the WeConnect RESTFUL API built using flask-restplus",
+          version=2)
 
 
