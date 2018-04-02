c59caa67656f5334535f7be2896484884644f6df
KASIGAZI BRIAN <kasigazibrian>
2018-04-02 12:09:34 +0300

    Adds swagger documentation to the Authentication end points

--- app/Authentication/views.py
+++ app/Authentication/views.py
@@ -1,10 +1,17 @@
 from flask import request, jsonify
-from flask_restful import Resource
+from flask_restplus import Resource, fields
 from app.app import app, api
 from werkzeug.security import generate_password_hash
 from app.models import User, Token, db
 import jwt
 from functools import wraps
+
+authorizations = {'api_key':{
+    'type': 'apiKey',
+    'in': 'header',
+    'name': 'access-token'
+
+}}
 
 
 def jwt_required(f):
@@ -23,35 +30,61 @@ def jwt_required(f):
                         my_token.tk_owner.login_status = False
                         my_token.blacklist = True
                         db.session.commit()
-                        return jsonify({'message': 'Your session has expired!. Please log in again.'})
+                        return {'message': 'Your session has expired!. Please log in again.'}, 401
 
                 else:
-                    return jsonify({"message":"Your session has expired!. Please login again"})
+                    return {"message":"Your session has expired!. Please login again"}, 401
             else:
-                return jsonify({'message': 'Token is missing'})
+                return {'message': 'Token is missing'}, 401
         else:
-            return jsonify({'message': 'Token is missing'})
-        return f(current_user, *args, **kwargs)
+            return {'message': 'Token is missing'}, 401
+        return f(*args, current_user, **kwargs)
     return decorated
+
+
+User_model = api.model('User',
+                       {'first_name': fields.String('First Name', required=True),
+                        'last_name': fields.String('Last Name', required=True),
+                        'username': fields.String('Username', required=True),
+                        'email': fields.String('Email', required=True),
+                        'gender': fields.String('Gender', required=True),
+                        'password': fields.String('Password', required=True)
+                        }
+                       )
+login_model = api.model('Login', {'username': fields.String('Username', required=True),
+                                  'password': fields.String('Password', required=True)
+                                  }
+                        )
+new_password_model = api.model('Reset_password', {'new_password': fields.String('New password')})
+
+api.authorizations = authorizations
 
 
 class Home(Resource):
     def get(self):
         """route to home page"""
-        return jsonify({'message':'You are Welcome!'})
+        return {'message':'You are Welcome!'}, 200
 
 
 class Register(Resource):
-    """class for register"""
+    """class for registering a user"""
+    @api.doc(responses={400: 'Bad Request', 200: 'Success', 201: 'Created'})
+    @api.expect(User_model)
     def post(self):
         """Method to register a user"""
-        new_user = request.get_json(force=True)
-        username = new_user.get('username')
-        password = new_user.get('password')
-        first_name = new_user.get('first_name')
-        last_name = new_user.get('last_name')
-        email = new_user.get('email')
-        gender = new_user.get('gender')
+        new_user = api.payload
+        if 'username' in new_user:
+            username = new_user['username']
+        if 'password' in new_user:
+            password = new_user['password']
+        if 'first_name' in new_user:
+            first_name = new_user['first_name']
+        if 'last_name' in new_user:
+            last_name = new_user['last_name']
+        if 'email' in new_user:
+            email = new_user['email']
+        if 'gender' in new_user:
+            gender = new_user['gender']
         if username and password and first_name and last_name and email and gender:
             if User.is_valid_email(email) is True:
                 if User.is_valid_gender(gender) is True:
@@ -60,66 +93,80 @@ class Register(Resource):
                         if not User.query.filter_by(email=email).first():
                             login_status = False
                             add_new_user = User(username=username, password=hashed_password, first_name=first_name,
-                                    last_name=last_name, email=email, gender=gender, login_status=login_status)
+                                                last_name=last_name, email=email, gender=gender,
+                                                login_status=login_status)
 
                             response = User.add_user(add_new_user)
-                            response.status_code = 201;
-                            return  response
+                            return response
                         else:
-                            return jsonify({"message":"Email address already exists"})
+                            return {"message": "Email address already exists"}, 400
                     else:
-                        return jsonify({'message':'User already exists'})
+                        return {'message': 'User already exists'}, 400
                 else:
-                    return jsonify({'message':'Invalid gender. Try Male, Female, M, F'})
+                    return {'message': 'Invalid gender. Try Male, Female, M, F'}, 400
             else:
-                return jsonify({"message": "Not a valid email address"})
+                return {"message": "Not a valid email address"}, 400
         else:
-            return jsonify({'message': 'All fields are required, that is username,'
-                                       ' password, first_name, last_name, email and gender'})
+            return {'message': 'All fields are required, that is username,'
+                               ' password, first_name, last_name, email and gender'}, 400
 
 
 class Login(Resource):
+    """Class for logging in a user"""
+    @api.expect(login_model)
+    @api.doc(responses={400: 'Bad Request', 200: 'Success', 201: 'Created'})
     def post(self):
         """Logs in a user"""
-        test_user = request.get_json(force=True)
+        test_user = api.payload
         username = test_user.get('username')
         password = test_user.get('password')
         if username and password:
-            response = User.login(username=username,password=password)
-            response.status_code = 201
-            return response
-
-        else:
-            return jsonify({"message": "Both username and password are required"})
-
-
-@app.route('/api/v2/auth/logout', methods=['POST'])
-@jwt_required
-def logout(current_user):
-    if current_user.login_status is True:
-        if 'access-token' in request.headers:
-            token = request.headers['access-token']
-            response = Token.blacklist_token(token=token,user=current_user.username)
+            response = User.login(username=username, password=password)
             return response
         else:
-            return jsonify({"message":"Token is missing"})
-    else:
-        return jsonify({"message":"Please first login to perform the action"})
+            return {"message": "Both username and password are required"}, 400
 
 
-@app.route('/api/v2/auth/reset-password', methods=['POST'])
-# route to the reset-password
-@jwt_required
-def reset_password(current_user):
-    """Reset user password"""
-    new_user_data = request.get_json(force=True)
-    new_password = new_user_data.get('password')
-    my_new_password = generate_password_hash(new_password, method='sha256')
-    response = User.password_reset(current_user, my_new_password)
-    return response
+class PasswordReset(Resource):
+    """Class for resetting a user password"""
+    @api.doc(responses={400: 'Bad Request', 200: 'Success', 201: 'Created', 401: 'Unauthorised', 500: 'Database Error'},
+             security='api_key')
+    @jwt_required
+    @api.expect(new_password_model)
+    def post(self, current_user):
+        """Reset user password post request"""
+        new_user_data = api.payload
+        if 'new_password' in new_user_data:
+            new_password = new_user_data['new_password']
+            if new_password:
+                my_new_password = generate_password_hash(new_password, method='sha256')
+                response = User.password_reset(current_user, my_new_password)
+                return response
+            else:
+                return {'message': 'Please enter the new password'}, 400
+        else:
+            return {'message': 'Please enter the new password'}, 400
 
 
-api.add_resource(Home, '/','/home')
-api.add_resource(Register, '/api/v2/auth/register')
+class Logout(Resource):
+    @api.doc(responses={400: 'Bad Request', 401: 'Unauthorised', 201: 'Created', 401: 'Unauthorised', 500: 'Database Error'},
+             security='api_key')
+    @jwt_required
+    def post(self, current_user):
+        """Method to logout a user"""
+        if current_user.login_status is True:
+            if 'access-token' in request.headers:
+                token = request.headers['access-token']
+                response = Token.blacklist_token(token=token, user=current_user.username)
+                return response
+            else:
+                return {"message": "Token is missing"}, 401
+        else:
+            return {"message": "Please first login to perform the action"}, 401
+
+
 api.add_resource(Login, '/api/v2/login')
-# api.add_resource(PasswordReset, '/api/v2/auth/reset-password')
+api.add_resource(Register, '/api/v2/auth/register')
+api.add_resource(Logout, '/api/v2/auth/logout')
+api.add_resource(Home, '/home')
+api.add_resource(PasswordReset, '/api/v2/auth/reset-password')
