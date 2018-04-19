18b11f322fdc44ffb007a6b50b40373a3d408dba
kasigazibrian <kasigazibrian@gmail.com>
2018-04-19 20:48:44 +0300

    Adds pep8 standard to the manage.py file

--- manage.py
+++ manage.py
@@ -3,13 +3,14 @@ import unittest
 from app.app import app
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from app.models import db
+from app.app import db
 
 
 migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+
 
 @manager.command
 def test():
